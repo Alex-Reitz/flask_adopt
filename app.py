79b64d4c1bdd6f0f4161956cf629cebff29f744e
Alex Reitz <alexreitz@Alexs-MacBook-Pro.local>
2021-03-06 13:03:05 -0500

    solution

--- app.py
+++ app.py
@@ -1,30 +1,58 @@
-from flask import Flask, request, render_template, flash, redirect
-from models import db, connect_db, Pet
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from forms import AddPetForm
+from models import db, connect_db, Pet
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///adopt"
+app.config['SECRET_KEY'] = "abcdef"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ihaveasecret'
 
-DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
+toolbar = DebugToolbarExtension(app)
+
 @app.route("/")
-def home_page():
-    return render_template("home.html")
+def list_pets():
+    pets = Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
 
 @app.route("/add", methods=["GET", "POST"])
-def add_form():
-
+def add_pet():
     form = AddPetForm()
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        new_pet = Pet(**data)
+        # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{new_pet.name} added.")
+        return redirect(url_for('list_pets'))
+    else:
+        return render_template("pet_add_form.html", form=form)
+
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def edit_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
 
     if form.validate_on_submit():
-        name = form.name.data
-        species = form.species.data
-        flash(f"Added {name} of {species}")
-        return redirect("/add")
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+        db.session.commit()
+        flash(f"{pet.name} updated.")
+        return redirect(url_for('list_pets'))
+
     else:
-        return render_template("add_form.html", form=form)
+        return render_template("pet_edit_form.html", form=form, pet=pet)
+
+@app.route("/api/pets/<int:pet_id>", methods=['GET'])
+def api_get_pet(pet_id):
+    """Return basic info about pet in JSON."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    info = {"name": pet.name, "age": pet.age}
+
+    return jsonify(info)
--- forms.py
+++ forms.py
@@ -1,9 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class AddPetForm(FlaskForm):
-    name=StringField("Pet Name")
-    species=StringField("Species name")
-    Photo_url=StringField("Photo URL")
-    age=FloatField("Age")
-    notes=StringField("Notes about this pet")
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()],
+    )
+    species = SelectField(
+        "Species",
+        choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")],
+    )
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+    age = IntegerField(
+        "Age",
+        validators=[Optional(), NumberRange(min=0, max=30)],
+    )
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional(), Length(min=10)],
+    )
+
+class EditPetForm(FlaskForm):
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional(), Length(min=10)],
+    )
+    available = BooleanField("Available?")
